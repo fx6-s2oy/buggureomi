9b542921b3b3253646d99fb9e6344389e3cdd9bc
s2oy <w2032@e-mirim.hs.kr>
2024-12-22 16:55:02 +0000

    feat: 공유 링크 복사 page url 수정 (#51)

--- src/constant/link.ts
+++ src/constant/link.ts
@@ -1,4 +1,4 @@
-export const PAGE_URL = "buggureomi.vercel.app";
+export const PAGE_URL = "https://buggureomi.vercel.app/answer?question=";
 
 export const SHARE_LINK_PARAM = {
   pageUrl: PAGE_URL,
