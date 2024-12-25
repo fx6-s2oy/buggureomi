28b9b3e081a9c2fe6e08a924f7cd21f8b8b4533c
s2oy <w2032@e-mirim.hs.kr>
2024-12-25 04:16:22 +0000

    Refactor: add token api result checking routine (#54)
    * refactor: add token api result and error checking routine
    
    * comment: add login logic case explanation

--- src/pages/oauth/index.tsx
+++ src/pages/oauth/index.tsx
@@ -16,22 +16,53 @@ export default function OAuth() {
   if (code) {
     memberAPI.getToken({ code }).then((res) => {
       const data = res.data;
-      if (res.data.status === "OK") {
-        if (data) {
-          tokenCookie.setCookie("accessToken", data.data.accessToken, 0.25);
-          tokenCookie.setCookie("refreshToken", data.data.refreshToken, 1);
+
+      try {
+        if (res.data.status === "OK") {
+          if (data.data) {
+            tokenCookie.setCookie("accessToken", data.data.accessToken, 0.25);
+            tokenCookie.setCookie("refreshToken", data.data.refreshToken, 1);
+          }
+
+          if (tokenCookie.getCookie("accessToken")) {
+            memberAPI.search().then((res) => {
+              const data = res.data;
+
+              if (data.status === "OK") {
+                // Case0: 토큰 발행 O & 유저 정보 호출 O
+                setUserInfo(data.data);
+                history.push("/main");
+              } else {
+                // Case1: 토큰 API O & 유저 정보 API X
+                throw new Error("Fail to get user information");
+              }
+            });
+          } else {
+            // Case2: 토큰 발행 O but 토큰 저장 실패
+            // e.g. 서버에선 200을 보냈으나 리턴된 토큰이 없는 경우
+            throw new Error(
+              "Token API call is 'OK', but there is no accessToken"
+            );
+          }
+        } else {
+          // Case3: 토큰 발행 X
+          throw new Error("Fail to receive user token");
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          if (error.message === "Fail to get user information") {
+            // Case 1
+            history.push("/main");
+          } else if (
+            // Case 2 & 3
+            error.message === "Fail to receive user token" ||
+            error.message ===
+              "Token API call is 'OK', but there is no accessToken"
+          ) {
+            history.push("/member-login");
+          }
         }
       }
-
-      memberAPI.search().then((res) => {
-        const data = res.data;
-
-        if (data.status === "OK") {
-          setUserInfo(data.data);
-        }
-
-        history.push("/main");
-      });
     });
   }
 
